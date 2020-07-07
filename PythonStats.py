f5588fd418dc69662f65436ad57817887db5111a
ekawamoto1 <eric.kawamoto@baxter-academy.org>
2020-07-07 12:40:01 -0400

    first pass at implementing GUI functionality

--- PythonStats.py
+++ PythonStats.py
@@ -1,6 +1,11 @@
 # PythonStats.py
+import sys
 import math
+import tkinter as tk
+from tkinter import *
+#from tkinter import messagebox
 
+print(sys.version)
 if __name__ == "__main__":  
     print("PythonStats is being run directly") 
 else:  
@@ -63,6 +68,7 @@ def PrintDataPoints(inList):
 
 
 def PrintOutStats(inList):
+    n = len(inList)
     avg = ComputeMean(inList)
     extremes = ComputeExtremes(inList)
     print("\nFor %d data point(s), " %n)
@@ -108,27 +114,97 @@ def GetDataPointsFromFile():
     return numlist
     
 
-print("Enter 1 for keyboard input, 2 for file input:", end = " ")
-mode = int(input())
+def PythonStatsGUIApp(arg):
+    def OK_cmd():
+        x = My_entry.get()
+        print("x = %s, arg = %s" %(x,arg))
+        root.withdraw()
+        if int(arg) == 1:
+            #print("process data points")
+            sList = x.split(",")
+            numlist = []
+            for xx in sList:
+                numlist.append(float(xx))
+            print(numlist)
+        elif int(arg) == 2:
+            #print("process file name")
+            fName = x
+            print("file name is %s" %fName)
+        quit()
+        
+    def Cancel_cmd():
+        quit()
+        
+    w = 500
+    h = 100
+    xp = 300
+    yp = 300
+    geom_str = "{}x{}+{}+{}".format(w,h,xp,yp)
+    #print(geom_str)
+    root = tk.Tk()
+    root.geometry(geom_str)
+    
+    if int(arg) == 1:    # input data points in text field
+        root.title("Data entry")
+        My_label = tk.Label(root,text="Enter data points:")
 
-if mode == 1:    # data entered from keyboard
-    print("When no more data is left to enter, simply hit return.")
-    numlist = GetDataPointsFromConsole()
+    elif int(arg) == 2:    # read data points from file
+        root.title("File name entry")
+        My_label = tk.Label(root,text="Enter path name of data file:")
+        
+    else:    # invalid command-line argument
+        root.title("Error")
+        My_label = tk.Label(root,text="invalid CL argument")
     
-elif mode == 2:    # data read from file
-    numlist = GetDataPointsFromFile()
+    My_entry = tk.Entry(root)    
+    OK_button = tk.Button(root,text="OK",command=OK_cmd)
+    Cancel_button = tk.Button(root,text="Cancel",command=Cancel_cmd)
 
-else:
-    print("Invalid mode; exiting program.")
-    quit()
+    My_label.place(x=0.5*w/10,y=2*h/8)
+    My_entry.place(x=4.5*w/10,y=2*h/8,height=30,width=5*w/10)
+    OK_button.place(x=w/2-100,y=5*h/8)
+    Cancel_button.place(x=w/2+50,y=5*h/8)
+    root.mainloop()
     
     
-n = len(numlist)
-if n > 0:
-    PrintDataPoints(numlist)
-    PrintOutStats(numlist)
+    
+
+def PythonStatsConsoleApp():
+    print("Enter 1 for keyboard input, 2 for file input:", end = " ")
+    mode = int(input())
+    
+    if mode == 1:    # data entered from keyboard
+        print("When no more data is left to enter, simply hit return.")
+        numlist = GetDataPointsFromConsole()
+        
+    elif mode == 2:    # data read from file
+        numlist = GetDataPointsFromFile()
+    
+    else:
+        print("Invalid mode; exiting program.")
+        quit()
+        
+    n = len(numlist)
+    if n > 0:
+        PrintDataPoints(numlist)
+        PrintOutStats(numlist)
+    else:
+        print("No data points to be analyzed.")
+
+
+# --------------------------------------------------
+# main body of PythonStats.py
+# must define functions above before this is executed
+nArgs = len(sys.argv)
+#print("nArgs = %d, sys.argv[0] = %s" %(nArgs, sys.argv[0]))
+if nArgs > 1:
+    #print("sys.argv[1] = %s" %(sys.argv[1]))
+    PythonStatsGUIApp(sys.argv[1])
 else:
-    print("No data points to be analyzed.")
+    PythonStatsConsoleApp()
+    
+# end main body of PythonStats.py    
+# --------------------------------------------------
 
 
 
--- README.md
+++ README.md
@@ -10,9 +10,20 @@ installing Python 3: [The right and wrong way to set Python 3 as default on a Ma
 
 use pyenv to run multiple versions of Python: [How to use pyenv to run multiple versions of Python on a Mac](https://opensource.com/article/20/4/pyenv)
 
-[pyenv Tutorial](https://amaral.northwestern.edu/resources/guides/pyenv-tutorial)
+tutorial on pyenv: [pyenv Tutorial](https://amaral.northwestern.edu/resources/guides/pyenv-tutorial)
 
 [__name__ == "__main__"](https://stackoverflow.com/questions/419163/what-does-if-name-main-do)
+
+installing tkinter [pyenv install doesn't work with homebrew installed tcl-tk #1375](https://github.com/pyenv/pyenv/issues/1375)
+
+using the Tk entry widget: [The Entry Widget](https://www.geeksforgeeks.org/python-tkinter-entry-widget/)
+
+more about Tkinter: [Hello, Tkinter](https://effbot.org/tkinterbook/tkinter-hello-tkinter.htm)
+
+## Running Journal
+Tue 07/07: added GUI functionality
+Mon 07/06: installed Python 3.8.3 after installing pyenv and Homebrew; implemented basic funcionality
+
 
 
 ## Contact
