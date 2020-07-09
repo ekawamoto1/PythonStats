58f593729afe34123a5fefe9338bf05cd3cc175c
ekawamoto1 <eric.kawamoto@baxter-academy.org>
2020-07-08 21:30:40 -0400

    changed GetDataPointsFromConsole() and PythonStatsConsoleApp() so that output matches JavaStats

--- PythonStats.py
+++ PythonStats.py
@@ -8,7 +8,8 @@ print(sys.version)
 if __name__ == "__main__":  
     print("PythonStats is being run directly") 
 else:  
-    print("PythonStats is being imported") 
+    print("PythonStats is being imported")
+print("")
     
 
 def ComputeExtremes(inList):
@@ -52,19 +53,19 @@ def ComputeMedian(inList):
 
 def PrintDataPoints(inList):
     m = len(inList)
+    outStr = ""
     if m > 0:
-        outStr = ""
         if m < 10:
             i = 0
             for y in inList:
                 i += 1
-                outStr += "\nData point {0:d}: {1:.2f}".format(i, y)
+                outStr += "Data point {0:d}: {1:.2f}\n".format(i, y)
         else:
             for i in range(0, 5):
-                outStr += "\nData point {0:d}: {1:.2f}".format(i+1, inList[i])
-            outStr += "\n    ..."
+                outStr += "Data point {0:d}: {1:.2f}\n".format(i+1, inList[i])
+            outStr += "     ...\n"
             for i in range(m - 5, m):
-                outStr += "\nData point {0:d}: {1:.2f}".format(i+1, inList[i])
+                outStr += "Data point {0:d}: {1:.2f}\n".format(i+1, inList[i])
                 
     return outStr
     
@@ -88,7 +89,11 @@ def PrintOutStats(inList):
 
 def GetDataPointsFromConsole():
     numlist = []
+    i = 0
     while True:
+        i += 1
+        outStr = "Data point {0:d}:".format(i)
+        print(outStr, end = " ")
         x = input()
         if x == "":
             break
@@ -102,7 +107,7 @@ def GetDataPointsFromConsole():
 def GetDataPointsFromFile(fName):
     numlist = []
     if fName == "":
-        print("Enter pathname of data file:", end = " ")
+        print("\nEnter pathname of data file:", end = " ")
         fName = input()
     try:
         f = open(fName, "r")
@@ -237,22 +242,22 @@ def PythonStatsConsoleApp():
     mode = int(input())
     
     if mode == 1:    # data entered from keyboard
-        print("When no more data is left to enter, simply hit return.")
+        print("\nWhen no more data is left to enter, simply hit return.")
         numlist = GetDataPointsFromConsole()
+        s = ""
         
     elif mode == 2:    # data read from file
         numlist = GetDataPointsFromFile("")
-    
+        s = PrintDataPoints(numlist)
+        
     else:
         print("Invalid mode; exiting program.")
         quit()
         
     n = len(numlist)
     if n > 0:
-        s1 = PrintDataPoints(numlist)
-        s2 = PrintOutStats(numlist)
-        s3 = s1 + "\n" + s2
-        print(s3)
+        s += PrintOutStats(numlist)
+        print(s)
     else:
         print("No data points to be analyzed.")
 
@@ -278,27 +283,26 @@ else:
 
 
 
+
 #  ----jGRASP exec: python3 PythonStats.py
+# 3.8.3 (default, Jul  7 2020, 02:06:50) 
+# [Clang 11.0.3 (clang-1103.0.32.62)]
 # PythonStats is being run directly
+# 
 # Enter 1 for keyboard input, 2 for file input: 1
+# 
 # When no more data is left to enter, simply hit return.
-# 6
-# 1
-# 5
-# 2
-# 4
-# 3
+# Data point 1: 6
+# Data point 2: 1
+# Data point 3: 5
+# Data point 4: 2
+# Data point 5: 4
+# Data point 6: 3
+# Data point 7: 
 # 
-# Data point 1: 6.00
-# Data point 2: 1.00
-# Data point 3: 5.00
-# Data point 4: 2.00
-# Data point 5: 4.00
-# Data point 6: 3.00
-# 
-# For 6 data point(s), 
+# For 6 data point(s)
 #     the maximum is 6.00
-#     the minimum is 1.00
+#     the maximum is 1.00
 #     the mean (average) is 3.50
 #     the median is 3.50
 #     the std dev is 1.87
@@ -306,28 +310,25 @@ else:
 #  ----jGRASP: operation complete.
 # 
 #  ----jGRASP exec: python3 PythonStats.py
+# 3.8.3 (default, Jul  7 2020, 02:06:50) 
+# [Clang 11.0.3 (clang-1103.0.32.62)]
 # PythonStats is being run directly
+# 
 # Enter 1 for keyboard input, 2 for file input: 1
+# 
 # When no more data is left to enter, simply hit return.
-# 7
-# 6
-# 5
-# 4
-# 3
-# 2
-# 1
+# Data point 1: 7
+# Data point 2: 6
+# Data point 3: 5
+# Data point 4: 4
+# Data point 5: 3
+# Data point 6: 2
+# Data point 7: 1
+# Data point 8: 
 # 
-# Data point 1: 7.00
-# Data point 2: 6.00
-# Data point 3: 5.00
-# Data point 4: 4.00
-# Data point 5: 3.00
-# Data point 6: 2.00
-# Data point 7: 1.00
-# 
-# For 7 data point(s), 
+# For 7 data point(s)
 #     the maximum is 7.00
-#     the minimum is 1.00
+#     the maximum is 1.00
 #     the mean (average) is 4.00
 #     the median is 4.00
 #     the std dev is 2.16
@@ -335,48 +336,59 @@ else:
 #  ----jGRASP: operation complete.
 # 
 #  ----jGRASP exec: python3 PythonStats.py
+# 3.8.3 (default, Jul  7 2020, 02:06:50) 
+# [Clang 11.0.3 (clang-1103.0.32.62)]
 # PythonStats is being run directly
+# 
 # Enter 1 for keyboard input, 2 for file input: 1
+# 
 # When no more data is left to enter, simply hit return.
-# 3.4
+# Data point 1: 3.4
+# Data point 2: 
 # 
-# Data point 1: 3.40
-# 
-# For 1 data point(s), 
+# For 1 data point(s)
 #     the maximum is 3.40
-#     the minimum is 3.40
+#     the maximum is 3.40
 #     the mean (average) is 3.40
 # 
 #  ----jGRASP: operation complete.
 # 
 #  ----jGRASP exec: python3 PythonStats.py
+# 3.8.3 (default, Jul  7 2020, 02:06:50) 
+# [Clang 11.0.3 (clang-1103.0.32.62)]
 # PythonStats is being run directly
-# Enter 1 for keyboard input, 2 for file input: 1
-# When no more data is left to enter, simply hit return.
 # 
+# Enter 1 for keyboard input, 2 for file input: 1
+# 
+# When no more data is left to enter, simply hit return.
+# Data point 1: 
 # No data points to be analyzed.
 # 
 #  ----jGRASP: operation complete.
 # 
 #  ----jGRASP exec: python3 PythonStats.py
+# 3.8.3 (default, Jul  7 2020, 02:06:50) 
+# [Clang 11.0.3 (clang-1103.0.32.62)]
 # PythonStats is being run directly
+# 
 # Enter 1 for keyboard input, 2 for file input: 2
+# 
 # Enter pathname of data file: testdata1.txt
 # Data point 1: 60.00
 # Data point 2: 62.00
 # Data point 3: 57.00
 # Data point 4: 58.00
 # Data point 5: 68.00
-#     ...
+#      ...
 # Data point 6: 65.00
 # Data point 7: 63.00
 # Data point 8: 59.00
 # Data point 9: 60.00
 # Data point 10: 58.00
 # 
-# For 10 data point(s), 
+# For 10 data point(s)
 #     the maximum is 68.00
-#     the minimum is 57.00
+#     the maximum is 57.00
 #     the mean (average) is 61.00
 #     the median is 60.00
 #     the std dev is 3.50
@@ -384,29 +396,41 @@ else:
 #  ----jGRASP: operation complete.
 # 
 #  ----jGRASP exec: python3 PythonStats.py
+# 3.8.3 (default, Jul  7 2020, 02:06:50) 
+# [Clang 11.0.3 (clang-1103.0.32.62)]
 # PythonStats is being run directly
+# 
 # Enter 1 for keyboard input, 2 for file input: 2
+# 
 # Enter pathname of data file: testdata2.txt
 # Data point 1: 23.50
 # 
-# For 1 data point(s), 
+# For 1 data point(s)
 #     the maximum is 23.50
-#     the minimum is 23.50
+#     the maximum is 23.50
 #     the mean (average) is 23.50
 # 
 #  ----jGRASP: operation complete.
 # 
 #  ----jGRASP exec: python3 PythonStats.py
+# 3.8.3 (default, Jul  7 2020, 02:06:50) 
+# [Clang 11.0.3 (clang-1103.0.32.62)]
 # PythonStats is being run directly
+# 
 # Enter 1 for keyboard input, 2 for file input: 2
+# 
 # Enter pathname of data file: testdata3.txt
 # No data points to be analyzed.
 # 
 #  ----jGRASP: operation complete.
 # 
 #  ----jGRASP exec: python3 PythonStats.py
+# 3.8.3 (default, Jul  7 2020, 02:06:50) 
+# [Clang 11.0.3 (clang-1103.0.32.62)]
 # PythonStats is being run directly
+# 
 # Enter 1 for keyboard input, 2 for file input: 2
+# 
 # Enter pathname of data file: testdata4.txt
 # File testdata4.txt does not exist.
 # 
